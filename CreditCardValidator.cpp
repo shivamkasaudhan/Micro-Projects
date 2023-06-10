fa0da82b885c6fde5077055a5b00cfe93b70e56f
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-06-10 19:51:21 +0530

    CC Number Validator Based on Lunh Algorithm

--- CreditCardValidator.cpp
+++ CreditCardValidator.cpp
@@ -44,10 +44,6 @@ int main() {
         for (int i = len - 1; i >= 0; i = i - 2) {
             doubleEvenSum += (ccNumber[i] - 48);
         }
-        
-        // Step 3 is to check if the final 'doubleEvenSum' is a multiple of 10.
-        // If yes, it is a valid CC number. Otherwise, not.
-        
         cout << (doubleEvenSum % 10 == 0 ? "Valid!" : "Invalid!") << endl;
         
         continue;        
